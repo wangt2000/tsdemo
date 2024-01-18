7d8fd2d1ef10056b868a28a9efaff01a946810a5
wangt2000 <1083492192@qq.com>
2024-01-19 00:09:07 +0800

    修改求和方法2

--- demo_fun.ts
+++ demo_fun.ts
@@ -6,8 +6,14 @@ function addNumber(numArr: number[]) {
     return sum
 }
 
-function addXxx(){
+function addNumber2(numArr: number[]) {
+    var sum = 0
+    for (const key in numArr) {
+        sum+=numArr[key]
+    }
     
+    return sum
 }
 
 console.log(addNumber([1, 2, 3, 4, 5, 6, 7]))
+console.log(addNumber2([1, 2, 3, 4, 5, 6, 7]))
