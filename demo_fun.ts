609445d1f0290ddc3eecf7a2dfb0b83a8c683bd1
wangt2000 <1083492192@qq.com>
2024-01-18 23:47:28 +0800

    Create demo_fun.ts
    新增方法练习

---
+++ demo_fun.ts
@@ -0,0 +1,9 @@
+function addNumber(numArr: number[]) {
+    var sum = 0
+    numArr.forEach(element => {
+        sum += element
+    });
+    return sum
+}
+
+console.log(addNumber([1, 2, 3, 4, 5, 6, 7]))
